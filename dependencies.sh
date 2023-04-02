cda85379933fa3b664991f4ad2f53862e60828a1
Blundir <blundir@gmail.com>
2023-04-02 16:54:17 +0300

    kill me (presumably ported tg icon/map merger)

--- dependencies.sh
+++ dependencies.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 #Project dependencies file
 #Final authority on what's required to fully build the project
 
 # byond version
-# Extracted from the Dockerfile. Change by editing Dockerfile's FROM command.
 export BYOND_MAJOR=514
-export BYOND_MINOR=1589
+export BYOND_MINOR=1588
 
 #rust_g git tag
 export RUST_G_VERSION=1.2.0
@@ -15,8 +14,14 @@ export RUST_G_VERSION=1.2.0
 export NODE_VERSION=14
 export NODE_VERSION_PRECISE=14.16.1
 
-# PHP version
-export PHP_VERSION=7.2
-
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.7.2
+export SPACEMAN_DMM_VERSION=suite-1.7.3
+
+# Python version for mapmerge and other tools
+export PYTHON_VERSION=3.9.0
+
+#auxlua repo
+export AUXLUA_REPO=tgstation/auxlua
+
+#auxlua git tag
+export AUXLUA_VERSION=1.4.0
